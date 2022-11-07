9fe22fa4b78801445ca815397941770a86e505bd
Tchinonso <lightersmercury@gmail.com>
2022-11-07 17:00:14 +0100

    blackjack in progress

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-var c
+var firstCard = 20
+var secondCard = 1
+var sum = firstCard + secondCard + 3
+console.log(sum)
+
+if (sum < 21) {
+	console.log("do you want to draw a new card? ")
+} else if (sum === 21) {
+	console.log("whoa, you've got blackjack ")
+} else {
+	console.log("you're out of the game ")
+}
+
+let age = 20
+
+if (age < 21) {
+	console.log("you can not enter the night club ")
+} else {
+	console.log("you're welcome ")
+}
